fb4af26e2b3c59e3c7a7404309ec550981331eb0
jackelynefarfan <farfanjackelyne9@gmail.com>
2021-06-13 18:03:34 -0500

    primer commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const btn1 = document.getElementById("btn1");
+btn1.addEventListener("click",() =>{
+ productos.style.display="none";
+  boxr.style.display="block";
+}) 
+const btn2 = document.getElementById("btn2");
+btn2.addEventListener("click",() =>{
+ productos.style.display="none";
+  boxs.style.display="block";
+}) 
+const btn3 = document.getElementById("btn3");
+btn3.addEventListener("click",() =>{
+ productos.style.display="none";
+  boxz.style.display="block";
+}) 
